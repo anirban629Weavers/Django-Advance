4d8b32e2a972741b350b1902386028c1056023c3
Anirban Mishra <anirban.mishra@weavers-web.com>
2023-11-24 19:48:13 +0530

    upto registration

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from home.views import home,contact,about
-from vege.views import recipes,delete_recipe,update_recipe
+from vege.views import recipes,delete_recipe,update_recipe,login_user,register_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +13,8 @@ urlpatterns = [
     path('recipes/', recipes,name="recipes"),
     path('delete-recipe/<id>/', delete_recipe,name="delete-recipes"),
     path('update-recipe/<id>/', update_recipe,name="delete-recipes"),
+    path('login/', login_user,name="login"),
+    path('register/', register_user,name="register"),
 ]
 
 urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Recipe
 from django.contrib import messages
+from django.contrib.auth.models import User 
 
 # Create your views here.
 def recipes(request):
@@ -20,6 +21,10 @@ def recipes(request):
         return redirect('recipes')
 
     all_recipes=Recipe.objects.all()
+    
+    if(request.GET.get('search')):
+         all_recipes=all_recipes.filter(recipe_name__icontains=request.GET.get('search'))
+    
     return render(request, 'vege/recipes.html',context={'recipes':all_recipes})
 
 def update_recipe(request,id):
@@ -49,3 +54,38 @@ def delete_recipe(request,id):
     recipe.delete()
     messages.success(request,"Recipe deleted successfully")
     return redirect('recipes')
+
+
+def login_user(request):
+    return render(request,'vege/login.html')
+
+def register_user(request):
+    if request.method=="POST":
+        data=request.POST
+        
+        first_name=data.get('first_name')
+        last_name=data.get('last_name')
+        username=data.get('username')
+        email=data.get('email')
+        password=data.get('password')
+        
+        user=User.objects.filter(email=email)
+        
+        if user.exists():    
+            messages.error(request,"Email already registered")
+            return render(request,'vege/register.html')
+        else:
+            user=User.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email
+            )
+            user.set_password(password)
+            user.save()
+            messages.success(request,"User Registered Successfully")
+            return redirect("/recipes")
+
+
+    
+    return render(request,'vege/register.html')
