2e36c4e1b232c7eec5725d9f9c7c4716d116387f
Anirban Mishra <anirban.mishra@weavers-web.com>
2023-11-24 12:29:45 +0530

    4 - template engine

--- home/views.py
+++ home/views.py
@@ -3,5 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/base.html')
-    
+    peoples=[
+        {"name": "Tiler","age": 49}, 
+        {"name": "Garwin","age": 100}, 
+        {"name": "Minda","age": 84}, 
+        {"name": "Mattie","age": 44}, 
+        {"name": "Rachael","age": 63}, 
+        {"name": "Doralynn","age": 24}, 
+        {"name": "Ashia","age": 19}, 
+        {"name": "Orsola","age": 47},
+        {"name": "Orsola","age": 14},
+        ]
+    return render(request, 'home/index.html',context={'peoples':peoples})
+
+def about(request):
+    return render(request, 'home/about.html',context={'title': 'About'})
+
+
+def contact(request):
+    return render(request, 'home/contact.html',context={'title': 'Contact'})
